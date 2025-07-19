3c18b53af316ae02d2194ee57afd521592844305
Ignacio-Licciardi <93827621+Ignacio-Licciardi@users.noreply.github.com>
2025-07-19 16:47:45 -0300

    CORS

--- src/main/java/com/AM/mvpAM/config/WebConfig.java
+++ src/main/java/com/AM/mvpAM/config/WebConfig.java
@@ -13,11 +13,13 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
 }
-
